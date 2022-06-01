8be8c5b644f817833989ecc33863808efdb20670
Kristin Kleyla <clever_disguises@yahoo.com>
2022-06-01 15:54:13 -0400

    setting up basic HTML file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,12 @@
-console.log('How do you do?');
+// TODO: Search for city, display weather conditions
+// TODO: City name, date, icon of weather conditions, temp, humidity, wind speed, UV index
+// TODO: Color code UV index
+// TODO: Future forecast: 5 days, date, icon of weather conditions, temp, wind speed, humidity
+// TODO: Search history to redisplay conditions (local storage)
+
+// TODO: init function
+// TODO: fetch geocoding API
+// TODO: fetch forecast API with latitude and longitude
+// TODO: display function to dynamically write to HTML
+// TODO: save searches to local storage to reaccess easily
+// TODO: Format page
