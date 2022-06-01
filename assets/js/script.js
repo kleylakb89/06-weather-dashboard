bfb86a47c501cdfc7732b7d1fd4e3d5e893cf81d
Kristin Kleyla <clever_disguises@yahoo.com>
2022-06-01 16:29:44 -0400

    figured out function to fetch weather data for city and change unit to imperial

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,6 +20,8 @@
 var searchEl = document.querySelector('#city-search');
 var submitEl = document.querySelector('.submit');
 
+var lat;
+var lon;
 
 function init() {
     
@@ -27,19 +29,31 @@ function init() {
 
 function citySearch() {
     var currentCity = searchEl.value;
-    var geoUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${currentCity}&limit=1&appid=43ba4285918e75abf5e651327d673253`
+    var geoUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${currentCity}&limit=1&appid=43ba4285918e75abf5e651327d673253`;
     
     fetch(geoUrl)
         .then(function (response) {
             return response.json();
         })
         .then(function (data) {
-            console.log(data)
             for (var city of data) {
-                var lat = city.lat;
-                var lon = city.lon;
+                lat = city.lat;
+                lon = city.lon;
             }
             console.log(lat, lon);
+            weatherSearch(lat, lon);
+        })
+}
+
+function weatherSearch(lat, lon) {
+    weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=43ba4285918e75abf5e651327d673253&units=imperial`;
+
+    fetch(weatherUrl)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
         })
 }
 
