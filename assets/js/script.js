92b7f72aaeca4edd36ee35ec554f7000ecac6612
Kristin Kleyla <clever_disguises@yahoo.com>
2022-06-02 20:40:02 -0400

    cleaned up and added comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,33 +1,15 @@
-// TODO: Search for city, display weather conditions
-// TODO: City name, date, icon of weather conditions, temp, humidity, wind speed, UV index
-// TODO: Color code UV index
-// TODO: Future forecast: 5 days, date, icon of weather conditions, temp, wind speed, humidity
-// TODO: Search history to redisplay conditions (local storage)
-
-// TODO: init function
-// TODO: fetch geocoding API
-// TODO: fetch forecast API with latitude and longitude
-// TODO: display function to dynamically write to HTML
-// TODO: save searches to local storage to reaccess easily
-// TODO: Format page
-
-// TODO: Add No Results Found for failed search
-
-// TODO: In order to search for a city:
-// query textarea and button
-// add event listener to button
-// fail state for searched city?
-// fetch API for geocode of city
-
+// query elements: textarea, search button, main element, section element
 var searchEl = document.querySelector('#city-search');
 var submitEl = document.querySelector('.submit');
 var cityEl = document.querySelector('.current-city');
 var historyEl = document.querySelector('.history');
 
+// start page function: will display search history from local storage
 function init() {
     displayHistory();
 }
 
+// accesses geocode API from OpenWeather and fetches search city's latitude and longitude
 function citySearch() {
     var currentCity = searchEl.value.trim();
     var geoUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${currentCity}&limit=1&appid=43ba4285918e75abf5e651327d673253`;
@@ -35,7 +17,7 @@ function citySearch() {
 
     fetch(geoUrl)
         .then(function (response) {
-            // Will only save the searched city if it gives a valid response from the API
+            // Will only return response if it gives a valid response from the API, else responds not found
             if (response.ok) {
                 return response.json();
             } else {
@@ -50,6 +32,7 @@ function citySearch() {
                     lat = city.lat;
                     lon = city.lon;
                 }
+                // saves the current city into local storage and passes off city name, lat, and lon to weatherSearch
                 searchHistory(currentCity);
                 weatherSearch(currentCity, lat, lon);
             } else {
@@ -62,18 +45,20 @@ function citySearch() {
         })
 }
 
-
+// uses parameters passed from citySearch to access OpenWeather's One Call API
 function weatherSearch(city, lat, lon) {
     var weatherUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=43ba4285918e75abf5e651327d673253&units=imperial`;
 
 
+    // clears innerHTML before posting new content
     cityEl.innerHTML = null;
 
     fetch(weatherUrl)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (data) {
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            // creates HTML elements to then assign content and append
             var currentEl = document.createElement('section');
             var nameEl = document.createElement('h2');
             var dateEl = document.createElement('h3');
@@ -85,11 +70,14 @@ function weatherSearch(city, lat, lon) {
             var uviEl = document.createElement('p');
             var spanEl = document.createElement('span');
             var fiveDayEl = document.createElement('p');
-            
+
+            // uses moment.js to capture the current date
             var now = moment().format('MM/DD/YYYY');
+            // pulls icon number from data object to use in icon URL
             var icon = data.current.weather[0].icon;
             var iconUrl = `http://openweathermap.org/img/wn/${icon}@2x.png`;
-            
+
+            // sets content to elements
             nameEl.textContent = city;
             dateEl.textContent = now;
             iconEl.src = iconUrl;
@@ -99,21 +87,24 @@ function weatherSearch(city, lat, lon) {
             uviEl.textContent = 'UVI: ';
             spanEl.textContent = data.current.uvi
             fiveDayEl.textContent = '5 Day Forecast: ';
-            
+
+            // adds class to divEl for css styling
             divEl.className = 'current-list';
-            
+
+            // sets background color of spanEl depending on value of uvi
             if (data.current.uvi < 4) {
                 spanEl.style.backgroundColor = 'green';
             } else if (data.current.uvi > 3 && data.current.uvi < 7) {
                 spanEl.style.backgroundColor = 'orange';
             } else span.style.backgroundColor = 'red';
 
+            // appends elements
             uviEl.appendChild(spanEl);
             divEl.append(tempEl, humidEl, windEl, uviEl);
             currentEl.append(nameEl, dateEl, iconEl);
             cityEl.append(currentEl, divEl, fiveDayEl);
 
-
+            // loops to create 5 forecast cards for next days utilizing futureBlock array of data in displayFuture function
             for (i = 0; i < 5; i++) {
                 var iconUrl = `http://openweathermap.org/img/wn/${icon}@2x.png`;
                 var day = i + 1;
@@ -129,6 +120,7 @@ function weatherSearch(city, lat, lon) {
         })
 }
 
+// creates, fills, and appends future forecast cards
 function displayFuture(arr) {
     var forecastEl = document.createElement('div');
     var fDateEl = document.createElement('h4');
@@ -149,6 +141,7 @@ function displayFuture(arr) {
     cityEl.appendChild(forecastEl);
 }
 
+// saves city searches in local storage
 function searchHistory(city) {
     var pastCities = JSON.parse(localStorage.getItem('search-history')) || [];
 
@@ -166,13 +159,17 @@ function searchHistory(city) {
 
     localStorage.setItem('search-history', JSON.stringify(pastCities))
 
+    // after saving, displays new history
     displayHistory();
 }
 
+// pulls search history from local storage and displays it
 function displayHistory() {
     var pastCities = JSON.parse(localStorage.getItem('search-history')) || [];
 
+    // clears innerHTML before displaying searches to avoid duplication
     historyEl.innerHTML = null;
+    // for of loop through the pastCities array and creating buttons for each city
     for (city of pastCities) {
         var pCityEl = document.createElement('button');
 
@@ -180,31 +177,32 @@ function displayHistory() {
 
         historyEl.append(pCityEl);
     }
+    // create clear history button at the bottom
     var clearEl = document.createElement('button');
     clearEl.textContent = 'Clear History';
     historyEl.appendChild(clearEl);
 }
 
+// event listener for clicking search button
 submitEl.addEventListener('click', citySearch);
 
+// event listener for recalling old searches
 historyEl.addEventListener('click', function (event) {
+    // targets which button is clicked and saves the name of the city from it
     var button = event.target
     var currentCity = button.textContent;
 
+    // if the button is the clear history button, clears innerHTML and local storage then returns
     if (currentCity === 'Clear History') {
         historyEl.innerHTML = null;
         localStorage.clear();
         return;
     }
+    // starts process of searching OpenWeather APIs for the saved city using previous fetch pattern
     var geoUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${currentCity}&limit=1&appid=43ba4285918e75abf5e651327d673253`;
-
-    searchHistory(currentCity);
-
     fetch(geoUrl)
         .then(function (response) {
-            // Will only save the searched city if it gives a valid response from the API
             if (response.ok) {
-                searchHistory(currentCity);
                 return response.json();
             } else {
                 (cityEl.textContent = 'Results Not Found');
@@ -228,4 +226,5 @@ historyEl.addEventListener('click', function (event) {
         })
 });
 
+// runs init function when page is accessed
 init();
